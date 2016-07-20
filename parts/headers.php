80b9dab3f14b67be5bbdfb18025246eb4d811930
Charles J Dillon <charlesjdillon@urel-mcit2001c.univrel.wsu.edu>
2016-07-20 09:49:59 -0700

    Few changes to the header and link color

--- parts/headers.php
+++ parts/headers.php
@@ -51,8 +51,9 @@ if ( true === spine_get_option( 'main_header_show' ) ) :
 	<div class="header-group hgroup guttered padded-bottom short">
 
 		<sup class="sup-header" data-section="<?php echo esc_attr( $spine_main_header_values['section_title'] ); ?>" data-pagetitle="<?php echo esc_attr( $spine_main_header_values['page_title'] ); ?>" data-posttitle="<?php echo esc_attr( $spine_main_header_values['post_title'] ); ?>" data-default="<?php echo esc_html( $spine_main_header_values['sup_header_default'] ); ?>" data-alternate="<?php echo esc_html( $spine_main_header_values['sup_header_alternate'] ); ?>"><span class="sup-header-default"><?php echo wp_kses_post( strip_tags( $spine_main_header_values['sup_header_default'], '<a>' ) ); ?></span></sup>
-		<sub class="sub-header" data-sitename="<?php echo esc_attr( $spine_main_header_values['site_name'] ); ?>" data-pagetitle="<?php echo esc_attr( $spine_main_header_values['page_title'] ); ?>" data-posttitle="<?php echo esc_attr( $spine_main_header_values['post_title'] ); ?>" data-default="<?php echo esc_html( $spine_main_header_values['sub_header_default'] ); ?>" data-alternate="<?php echo esc_html( $spine_main_header_values['sub_header_alternate'] ); ?>"><span class="sub-header-default"><?php echo wp_kses_post( strip_tags( $spine_main_header_values['sub_header_default'], '<a>' ) ); ?></span></sub>
-
+		<sub class="sub-header" data-sitename="<?php echo esc_attr( $spine_main_header_values['site_name'] ); ?>" data-pagetitle="<?php echo esc_attr( $spine_main_header_values['page_title'] ); ?>" data-posttitle="<?php echo esc_attr( $spine_main_header_values['post_title'] ); ?>" data-default="<?php echo esc_html( $spine_main_header_values['sub_header_default'] ); ?>" data-alternate="<?php echo esc_html( $spine_main_header_values['sub_header_alternate'] ); ?>"><span class="sub-header-default"><?php echo wp_kses_post( strip_tags( $spine_main_header_values['sub_header_default'], '<a>' ) ); ?>
+    </span>
+    </sub>
 	</div>
 </header>
 
@@ -66,21 +67,6 @@ if ( is_front_page() && ! is_home() && true === spine_get_option( 'front_page_ti
 		<h1><?php the_title(); ?></h1>
 	</div>
 </section>
-<?php
-endif;
 
-if ( is_home() && ! is_front_page() && true === spine_get_option( 'page_for_posts_title' ) ) :
-	$page_for_posts_id = get_option( 'page_for_posts' );
-	if ( $page_for_posts_id ) {
-		$page_for_posts_title = get_the_title( $page_for_posts_id );
-	} else {
-		$page_for_posts_title = '';
-	}
-	?>
-<section class="row single gutter pad-ends">
-	<div class="column one">
-		<h1><?php echo esc_html( $page_for_posts_title ); ?></h1>
-	</div>
-</section>
 <?php
 endif;
